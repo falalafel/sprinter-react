41417e62e4171f12bc1730036dc5b6ba2d34ef3c
Mateusz Szpyrka <mjsz1701@gmail.com>
2019-05-09 16:37:54 +0200

    Completed first version of AddProject component

--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -1,16 +1,87 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import {withStyles} from '@material-ui/core/styles/index';
+import ProjectMembersCreate from "./ProjectMembersCreate";
+import api from "../api";
+
+const styles = {
+    root: {
+        
+    },
+};
 
 class AddProject extends React.Component {
-    render() {
-        return (
-            <div>
 
-                //TODO
+    state = {
+        usersList: [],
+        members: [],
+    };
+
+    componentDidMount() {
+        this.fetchAndSetUsers()
+        console.log(this.state.usersList)
+    }
+
+    fetchAndSetUsers() {
+        api.fetch(
+            api.endpoints.getUsers().path,
+            (response) => {
+                this.setState({usersList: response})
+            });
+    }
+
+    toggleScrumMaster = (userId) => {
+        console.log("toggle: ", userId)
+
+        this.setState({members: this.state.members.map(
+            user => user.userId === userId ? ({
+                ...user,
+                isScrumMaster: !user.isScrumMaster
+            }) : user
+        )})
+    }
+
+    removeMember = (userId) => {
+        console.log("remove: ", userId)
+
+        this.setState({members: this.state.members.filter(
+            user => userId != user.userId
+        )})
+    }
+
+    addMember = (userId) => {
+        console.log("add: ", userId)
+
+        const newMember = this.state.usersList.find(u => u.userId === userId)
+        this.setState({members: [newMember].concat(this.state.members)})
+    }
+
+    render() {
+        const { classes } = this.props;
+        const { members, usersList } = this.state;
+
+        const notMembers = usersList.filter(user => (
+           !members.map(m => m.userId).includes(user.userId)
+        ))
+
+        return (
+            <div className={classes.root}>
+
+                <ProjectMembersCreate
+                    members={members}
+                    notMembers={notMembers}
+                    addMemberCallback={ (userId) => this.addMember(userId) }
+                    toggleScrumMasterCallback={ (userId) => this.toggleScrumMaster(userId) }
+                    removeMemberCallback={ (userId) => this.removeMember(userId) }
+                />
 
             </div>
-        )
+        );
     }
 }
 
+AddProject.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
 
-export default AddProject;
+export default withStyles(styles)(AddProject);
