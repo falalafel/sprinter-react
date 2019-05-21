672adeb7deba981b1faecff01bbef5ce479d178a
Klooskie <kuba.koniecznyxx@gmail.com>
2019-05-22 00:09:39 +0200

    Removed redundant BrowserRouter tag from App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
 import SprinterAppBar from './SprinterAppBar';
 import AddProject from './AddProject';
 import CloseSprint from './CloseSprint';
@@ -10,15 +9,14 @@ import DeclareHours from './DeclareHours';
 class App extends React.Component {
     render() {
         return (
-
-            <Router>
+            <div>
                 <Route strict path='/' component={SprinterAppBar}/>
                 <Route exact strict path='/overview' component={Overview}/>
                 <Route exact strict path='/new-project' component={AddProject}/>
                 <Route exact strict path='/declare-hours/project=:projectId/sprint=:sprintId' component={DeclareHours}/>
                 <Route exact strict path='/close-sprint/project=:projectId/sprint=:sprintId' component={CloseSprint}/>
                 <Route exact strict path='/manage-project/projectId=:projectid'/>  {/*TODO: component={ManageProject}/>*/}
-            </Router>
+            </div>
         );
     }
 }
