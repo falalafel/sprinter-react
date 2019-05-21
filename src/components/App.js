3fe808b61daa23132a7028d2de30f6c7c72a4703
Mateusz Szpyrka <mjsz1701@gmail.com>
2019-05-21 23:53:08 +0200

    Fixed routes structure

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,20 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import SprinterAppBar from './SprinterAppBar';
 import AddProject from './AddProject';
 import CloseSprint from './CloseSprint';
-import CloseProject from './CloseProject';
 import Overview from './Overview';
 import DeclareHours from './DeclareHours';
-import history from '../history';
 
 class App extends React.Component {
     render() {
         return (
 
-            <Router history={history}>
+            <Router>
                 <Route strict path='/' component={SprinterAppBar}/>
                 <Route exact strict path='/overview' component={Overview}/>
                 <Route exact strict path='/new-project' component={AddProject}/>
-                <Route exact strict path='/declare-hours/:projectid/:sprintid' component={DeclareHours}/>
-                <Route exact strict path='/close-sprint/:projectid/:sprintid' component={CloseSprint}/>
-                <Route exact strict path='/close-project/:projectid' component={CloseProject}/>
+                <Route exact strict path='/declare-hours/project=:projectId/sprint=:sprintId' component={DeclareHours}/>
+                <Route exact strict path='/close-sprint/project=:projectId/sprint=:sprintId' component={CloseSprint}/>
+                <Route exact strict path='/manage-project/projectId=:projectid'/>  {/*TODO: component={ManageProject}/>*/}
             </Router>
         );
     }
