c29b5e6ecdebd8a4484817c17f37e2547ae78722
Mateusz Szpyrka <mjsz1701@gmail.com>
2019-05-21 18:36:56 +0200

    Implemented sprint selection component

---
+++ src/components/SprintSelect.js
@@ -0,0 +1,260 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Select from 'react-select';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import NoSsr from '@material-ui/core/NoSsr';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import MenuItem from '@material-ui/core/MenuItem';
+import { emphasize } from '@material-ui/core/styles/colorManipulator';
+import {Button, ListItemText} from "@material-ui/core";
+import AddIcon from '@material-ui/icons/Add';
+import NotInterestedIcon from '@material-ui/icons/NotInterested';
+import IconButton from "@material-ui/core/IconButton";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import Tooltip from 'recharts/lib/component/Tooltip';
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+        maxWidth: 500,
+    },
+    input: {
+        display: 'flex',
+        padding: 0,
+    },
+    valueContainer: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        flex: 1,
+        alignItems: 'center',
+        overflow: 'hidden',
+    },
+    chip: {
+        margin: `${theme.spacing.unit / 2}px ${theme.spacing.unit / 4}px`,
+    },
+    chipFocused: {
+        backgroundColor: emphasize(
+            theme.palette.type === 'light' ? theme.palette.grey[300] : theme.palette.grey[700],
+            0.08,
+        ),
+    },
+    option: {
+        fontSize: 14,
+    },
+    noOptionsMessage: {
+        padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`,
+    },
+    singleValue: {
+        fontSize: 14,
+    },
+    placeholder: {
+        position: 'absolute',
+        left: 10,
+        fontSize: 14,
+    },
+    paper: {
+        position: 'absolute',
+        zIndex: 1,
+        marginTop: 0,
+        left: 0,
+        right: 0,
+    },
+
+});
+
+function NoOptionsMessage(props) {
+    return (
+        <Typography
+            color="textSecondary"
+            className={props.selectProps.classes.noOptionsMessage}
+            {...props.innerProps}
+        >
+            {props.children}
+        </Typography>
+    );
+}
+
+function inputComponent({ inputRef, ...props }) {
+    return <div style={{height: 50}} ref={inputRef} {...props} />;
+}
+
+function Control(props) {
+    return (
+        <TextField
+            fullWidth
+            InputProps={{
+                inputComponent,
+                inputProps: {
+                    className: props.selectProps.classes.input,
+                    inputRef: props.innerRef,
+                    children: props.children,
+                    ...props.innerProps,
+                },
+            }}
+            {...props.selectProps.textFieldProps}
+        />
+    );
+}
+
+function Option(props) {
+    return (
+        <MenuItem
+            buttonRef={props.innerRef}
+            selected={props.isFocused}
+            component="div"
+            className={props.selectProps.classes.option}
+            style={{
+                background: props.isSelected ? 'primary' : 'secondary',
+            }}
+            {...props.innerProps}
+        >
+            {props.children}
+
+        </MenuItem>
+
+    );
+}
+
+const getOptionLabel = (option) => {
+    return option.startDate;
+};
+
+const formatOptionLabel = option => (
+    <div>
+        <b>Sprint {option.id}: </b> {option.startDate}
+        <ListItemSecondaryAction>
+            {option.closingStatus === true ?
+                <Tooltip disableFocusListener disableTouchListener title="Sprint is closed" style={{ float: "right", padding: 10 }}>
+                    <NotInterestedIcon color='disabled' fontSize='small'/>
+                </Tooltip>
+                : "" }
+        </ListItemSecondaryAction>
+    </div>
+);
+
+const customFilterOption = (option, rawInput) => {
+
+    return true;
+
+    const words = rawInput.toUpperCase().split(' ');
+
+    const labelWords = option.label.toUpperCase().split(' ');
+
+    return words.reduce(
+        (acc, cur) => acc && (option.startDate.startsWith(cur)),// TODO: endDate || option.endDate.startsWith(cur)),
+        true,
+    );
+};
+
+
+function Placeholder(props) {
+    return (
+        <Typography
+            color="textSecondary"
+            className={props.selectProps.classes.placeholder}
+            {...props.innerProps}
+        >
+            {props.children}
+        </Typography>
+    );
+}
+
+function SingleValue(props) {
+    return (
+        <div><b>Sprint {props.data.id}: </b> {props.data.startDate}</div>
+    );
+}
+
+function Menu(props) {
+    return (
+        <Paper square className={props.selectProps.classes.paper} {...props.innerProps}>
+            {props.children}
+        </Paper>
+    );
+}
+
+function getSortedSprints(sprints) {
+    return sprints.slice().sort((a, b) => {
+
+        if (a.closingStatus === false && b.closingStatus === true)
+            return 1;
+
+        if (a.closingStatus === true && b.closingStatus === false)
+            return -1;
+
+        const dateA = new Date(a.startDate);
+        const dateB = new Date(b.startDate);
+
+        if (dateA === dateB)
+            return a.id - b.id;
+
+        return dateA - dateB;
+    }).reverse();
+}
+
+const components = {
+    Control,
+    Menu,
+    NoOptionsMessage,
+    Option,
+    Placeholder,
+    SingleValue,
+};
+
+class SprintSelect extends React.Component {
+
+    handleChange = selectedSprint => {
+        if(selectedSprint === null)
+            this.props.sprintChangeCallback(null)
+        else
+            this.props.sprintChangeCallback(selectedSprint.id);
+    };
+
+    findSprint = (selectedSprintId) => {
+        const selectedSprint = this.props.sprints.find(sprint => sprint.id === selectedSprintId)
+        return selectedSprint === undefined ? null : selectedSprint
+    };
+
+    render() {
+        const { classes, sprints, selectedSprintId } = this.props;
+        const sortedSprints = getSortedSprints(sprints)
+        const selectedSprint = this.findSprint(selectedSprintId);
+
+        return (
+            <div className={classes.root}>
+                <NoSsr>
+                    <Select
+                        classes={classes}
+                        options={sortedSprints}
+                        components={components}
+                        value={selectedSprint}
+                        onChange={this.handleChange}
+                        placeholder="Start typing sprint starting date..."
+                        isClearable
+                        formatOptionLabel={formatOptionLabel}
+                        getOptionLabel={getOptionLabel}
+                        filterOption={customFilterOption}
+                    />
+                </NoSsr>
+            </div>
+        );
+    }
+}
+
+SprintSelect.propTypes = {
+    classes: PropTypes.object.isRequired,
+    theme: PropTypes.object.isRequired,
+    sprints: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number,
+            startDate: PropTypes.date,
+            closingStatus: PropTypes.bool,
+        })
+    ).isRequired,
+    sprintChangeCallback: PropTypes.func,
+    selectedSprintId: PropTypes.number,
+};
+
+export default withStyles(styles, { withTheme: true })(SprintSelect);
