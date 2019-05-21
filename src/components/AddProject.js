8e5574aad7b978cb885b66537e8a94ab4e6c76ef
Mateusz Szpyrka <mjsz1701@gmail.com>
2019-05-21 11:33:28 +0200

    Implemented adding new project with members functionality

--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -6,6 +6,7 @@ import api from "../api";
 import ProjectConfig from "./ProjectConfig";
 import {Grid, Typography} from "@material-ui/core";
 import Button from "@material-ui/core/Button";
+import { resolve } from 'dns';
 
 const styles = (theme) => ({
     root: {
@@ -127,11 +128,30 @@ class AddProject extends React.Component {
             sprintDuration: this.state.sprintLength,
         };
 
+        // first, adds new project
         api.fetch(
             api.endpoints.createProject(data),
-            () => {
-                this.props.redirectToDashboardCallback();
-            });
+
+            // after the project is added, adds membership for all declared users
+            (newProjectId) => {
+
+                Promise.all(this.state.members.map(member => {
+
+                    api.fetchNoContent(api.endpoints.setProjectMembership(
+                        newProjectId,
+                        member.userId,
+                        {isScrumMaster: member.isScrumMaster}
+                    ),
+                    () => {
+                        return new Promise(() => resolve())
+                    })
+                })).then(
+
+                    // when all members are added successfully, redirects to overview for the project
+                    (result) => this.props.history.push(`/overview/${newProjectId}/`)
+                )
+            }
+        );
     };
 
     render() {
