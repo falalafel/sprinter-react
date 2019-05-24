81abd9f882c9d53b920e6dd65f49f363d49acede
Mateusz <mat646@gmail.com>
2019-05-24 14:25:36 +0200

    Authorization component draft

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,13 @@ import {Route} from 'react-router-dom';
 import SprinterAppBar from './SprinterAppBar';
 import AddProject from './AddProject';
 import Overview from './Overview';
+import SignIn from "./SignIn";
 
 class App extends React.Component {
     render() {
         return (
             <div>
+                <Route exact strict path='/signin' component={SignIn}/>
                 <Route strict path='/' component={SprinterAppBar}/>
                 <Route exact strict path='/overview' component={Overview}/>
                 <Route exact strict path='/new-project' component={AddProject}/>
