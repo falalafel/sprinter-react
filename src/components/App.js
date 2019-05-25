d7c825b0eaf50ffa309649bed613e13c3bfb56c8
Klooskie <kuba.koniecznyxx@gmail.com>
2019-05-25 12:41:59 +0200

    Fixed appbar appearing on sign-in page

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import SprinterAppBar from './SprinterAppBar';
 import AddProject from './AddProject';
 import Overview from './Overview';
 import SignIn from "./SignIn";
 
+
 class App extends React.Component {
     render() {
         return (
             <div>
-                <Route exact strict path='/signin' component={SignIn}/>
-                <Route strict path='/' component={SprinterAppBar}/>
+                <Switch>
+                    <Route exact strict path='/sign-in' component={SignIn}/>
+                    <Route strict path='/' component={SprinterAppBar}/>
+                </Switch>
+
                 <Route exact strict path='/overview' component={Overview}/>
                 <Route exact strict path='/new-project' component={AddProject}/>
                 <Route exact strict path='/manage-project/projectId=:projectid'/>  {/*TODO: component={ManageProject}/>*/}
+
             </div>
         );
     }
