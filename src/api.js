5cbbad136f9c2da62c3874bb3ca9bb3607efbcda
Mateusz <mat646@gmail.com>
2019-05-27 23:11:54 +0200

    https support

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 const host = "localhost";
 const port = 8080;
-const url = endpoint => `http://sprinter-api.herokuapp.com/${endpoint}`;
+const url = endpoint => `https://sprinter-api.herokuapp.com/${endpoint}`;
 let base64 = require('base-64');
 
 export default {
