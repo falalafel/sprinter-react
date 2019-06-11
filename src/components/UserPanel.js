a308de9d2eea46b8311fd80f876eaf62cd3f8be6
Klooskie <kuba.koniecznyxx@gmail.com>
2019-06-11 21:40:03 +0200

    Added basic user panel looks

--- src/components/UserPanel.js
+++ src/components/UserPanel.js
@@ -4,9 +4,14 @@ import {withStyles} from '@material-ui/core/styles/index';
 import ProjectMembersCreate from "./ProjectMembersCreate";
 import api from "../api";
 import ProjectConfig from "./ProjectConfig";
-import {Grid, Typography} from "@material-ui/core";
+import {Divider, Grid, Typography} from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import {resolve} from 'dns';
+import List from "@material-ui/core/List";
+import {userRole} from "../userRole";
+import EditIcon from '@material-ui/icons/Edit';
+import IconButton from '@material-ui/core/IconButton/index';
+
 
 const styles = (theme) => ({
     root: {},
@@ -15,31 +20,45 @@ const styles = (theme) => ({
         textAlign: 'center',
         padding: 40
     },
-    projectConfig: {
-        float: 'right',
-        marginRight: '4%',
+    descriptionColumn: {
+        textAlign: 'right',
+        color: 'gray',
     },
-    projectMembers: {
+    descriptionTypography: {
+        height: 70,
+        marginRight: 20,
+        verticalAlign: 'text-bottom',
+        paddingTop: 7,
+    },
+    dataColumn: {
         float: 'left',
-        marginLeft: '4%',
     },
-    addProjectButton: {
-        marginTop: theme.spacing.unit * 3,
-        width: 400,
-        height: 50,
+    dataTypography: {
+        float: 'left',
+        marginRight: 15,
+        height: 70,
+        // marginLeft: -100,
+        verticalAlign: 'text-bottom',
     },
-    close: {
-        padding: theme.spacing.unit / 2,
+    userData: {
+        // float: 'center',
+        borderStyle: 'dotted',
     },
+
+    button: {
+        float: 'left',
+    },
+    editIcon: {
+
+    },
+
 });
 
 class UserPanel extends React.Component {
 
     state = {
+        user: undefined,
         loading: true,
-        name: '',
-        mail: '',
-
     };
 
     componentDidMount() {
@@ -51,13 +70,11 @@ class UserPanel extends React.Component {
     }
 
     fetchAndSetUser() {
-        localStorage.getItem('user')
         api.fetch(
             api.endpoints.getUserById(this.getUserId()),
             (response) => {
                 this.setState({
-                    name: response.name,
-                    mail: response.mail,
+                    user: response,
                     loading: false
                 })
             });
@@ -66,16 +83,65 @@ class UserPanel extends React.Component {
     render() {
 
         const {classes} = this.props;
+        const {loading, user} = this.state;
 
-        const {
-            loading, name, mail
-        } = this.state;
+        console.log(user);
 
         return (
             <div className={classes.root}>
-                {loading ?
-                <Typography>...</Typography>:
-                <Typography>{name}, {mail}</Typography>
+                {loading
+                    ? <Typography>...</Typography>
+                    : <div>
+                        <div className={classes.title}>
+                            <Typography variant="h3">
+                                Your profile
+                            </Typography>
+                        </div>
+
+                        <div className={classes.userData}>
+                            <Grid container spacing={0} justify='center'>
+                                <Grid item xs={6}>
+                                    <div className={classes.descriptionColumn}>
+                                        <Typography variant='h5' className={classes.descriptionTypography}>Full name:</Typography>
+                                    </div>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <div className={classes.dataColumn}>
+                                        <Typography className={classes.dataTypography} variant='h4'>{user.name}</Typography>
+                                        <IconButton size="small" className={classes.button} title='Edit full name'>
+                                            <EditIcon fontSize="small" className={classes.editIcon} color={'disabled'}/>
+                                        </IconButton>
+                                    </div>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <div className={classes.descriptionColumn}>
+                                        <Typography variant='h5' className={classes.descriptionTypography}>E-mail:</Typography>
+                                    </div>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <div className={classes.dataColumn}>
+                                        <Typography className={classes.dataTypography} variant={'h4'}>{user.mail}</Typography>
+                                        <IconButton size="small" className={classes.button} title='Edit e-mail'>
+                                            <EditIcon fontSize="small" className={classes.editIcon} color={'disabled'}/>
+                                        </IconButton>
+                                    </div>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <div className={classes.descriptionColumn}>
+                                        <Typography variant='h5' className={classes.descriptionTypography}>Account type:</Typography>
+                                    </div>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <div className={classes.dataColumn}>
+                                        { user.role === userRole.ADMIN
+                                            ? <Typography className={classes.dataTypography} variant='h4'>administrator</Typography>
+                                            : <Typography className={classes.dataTypography} variant='h4'>standard</Typography>
+                                        }
+                                    </div>
+                                </Grid>
+                            </Grid>
+                        </div>
+                    </div>
             }
             </div>
         );
