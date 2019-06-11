999d7dfcb5d6bdbfe91554c962e18ea68b4ff202
Klooskie <kuba.koniecznyxx@gmail.com>
2019-06-11 22:13:19 +0200

    Fixed user data not being centered properly

--- src/components/UserPanel.js
+++ src/components/UserPanel.js
@@ -11,7 +11,7 @@ import List from "@material-ui/core/List";
 import {userRole} from "../userRole";
 import EditIcon from '@material-ui/icons/Edit';
 import IconButton from '@material-ui/core/IconButton/index';
-
+import BeatLoader from 'react-spinners/BeatLoader';
 
 const styles = (theme) => ({
     root: {},
@@ -20,37 +20,36 @@ const styles = (theme) => ({
         textAlign: 'center',
         padding: 40
     },
+    userData: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
     descriptionColumn: {
+        float: 'left',
         textAlign: 'right',
-        color: 'gray',
+        marginTop: 7,
     },
     descriptionTypography: {
         height: 70,
         marginRight: 20,
-        verticalAlign: 'text-bottom',
-        paddingTop: 7,
     },
     dataColumn: {
         float: 'left',
     },
+    dataLine: {
+        clear: 'left',
+    },
     dataTypography: {
         float: 'left',
-        marginRight: 15,
         height: 70,
-        // marginLeft: -100,
-        verticalAlign: 'text-bottom',
-    },
-    userData: {
-        // float: 'center',
-        borderStyle: 'dotted',
+        marginRight: 15,
     },
 
     button: {
         float: 'left',
     },
-    editIcon: {
-
-    },
+    editIcon: {},
 
 });
 
@@ -90,8 +89,14 @@ class UserPanel extends React.Component {
         return (
             <div className={classes.root}>
                 {loading
-                    ? <Typography>...</Typography>
-                    : <div>
+                    ?
+                    <BeatLoader
+                        loading={loading}
+                        size={10}
+                        color={'#0000f0'}
+                    />
+                    :
+                    <div>
                         <div className={classes.title}>
                             <Typography variant="h3">
                                 Your profile
@@ -99,50 +104,38 @@ class UserPanel extends React.Component {
                         </div>
 
                         <div className={classes.userData}>
-                            <Grid container spacing={0} justify='center'>
-                                <Grid item xs={6}>
-                                    <div className={classes.descriptionColumn}>
-                                        <Typography variant='h5' className={classes.descriptionTypography}>Full name:</Typography>
-                                    </div>
-                                </Grid>
-                                <Grid item xs={6}>
-                                    <div className={classes.dataColumn}>
-                                        <Typography className={classes.dataTypography} variant='h4'>{user.name}</Typography>
-                                        <IconButton size="small" className={classes.button} title='Edit full name'>
-                                            <EditIcon fontSize="small" className={classes.editIcon} color={'disabled'}/>
-                                        </IconButton>
-                                    </div>
-                                </Grid>
-                                <Grid item xs={6}>
-                                    <div className={classes.descriptionColumn}>
-                                        <Typography variant='h5' className={classes.descriptionTypography}>E-mail:</Typography>
-                                    </div>
-                                </Grid>
-                                <Grid item xs={6}>
-                                    <div className={classes.dataColumn}>
-                                        <Typography className={classes.dataTypography} variant={'h4'}>{user.mail}</Typography>
-                                        <IconButton size="small" className={classes.button} title='Edit e-mail'>
-                                            <EditIcon fontSize="small" className={classes.editIcon} color={'disabled'}/>
-                                        </IconButton>
-                                    </div>
-                                </Grid>
-                                <Grid item xs={6}>
-                                    <div className={classes.descriptionColumn}>
-                                        <Typography variant='h5' className={classes.descriptionTypography}>Account type:</Typography>
-                                    </div>
-                                </Grid>
-                                <Grid item xs={6}>
-                                    <div className={classes.dataColumn}>
-                                        { user.role === userRole.ADMIN
-                                            ? <Typography className={classes.dataTypography} variant='h4'>administrator</Typography>
-                                            : <Typography className={classes.dataTypography} variant='h4'>standard</Typography>
-                                        }
-                                    </div>
-                                </Grid>
-                            </Grid>
+
+                            <div className={classes.descriptionColumn}>
+                                <Typography variant='h5' className={classes.descriptionTypography}>Full name:</Typography>
+                                <Typography variant='h5' className={classes.descriptionTypography}>E-mail:</Typography>
+                                <Typography variant='h5' className={classes.descriptionTypography}>Account type:</Typography>
+                            </div>
+
+                            <div className={classes.dataColumn}>
+                                <div className={classes.dataLine}>
+                                    <Typography className={classes.dataTypography} variant='h4'>{user.name}jhvadsbajasdsadasdasdasdasdasdsadasdasdasdasdasdsadsad</Typography>
+                                    <IconButton size="small" className={classes.button} title='Edit full name'>
+                                        <EditIcon fontSize="small" className={classes.editIcon} color={'disabled'}/>
+                                    </IconButton>
+                                </div>
+                                <div className={classes.dataLine}>
+                                    <Typography className={classes.dataTypography}
+                                                variant={'h4'}>{user.mail}</Typography>
+                                    <IconButton size="small" className={classes.button} title='Edit e-mail'>
+                                        <EditIcon fontSize="small" className={classes.editIcon} color={'disabled'}/>
+                                    </IconButton>
+                                </div>
+                                <div className={classes.dataLine}>
+                                    {user.role === userRole.ADMIN
+                                        ? <Typography className={classes.dataTypography} variant='h4'>administrator</Typography>
+                                        : <Typography className={classes.dataTypography} variant='h4'>standard</Typography>
+                                    }
+                                </div>
+                            </div>
+
                         </div>
                     </div>
-            }
+                }
             </div>
         );
     }
