f314aafc278233dabd86390dc5310b03d3848239
Mateusz Szpyrka <mjsz1701@gmail.com>
2019-06-14 14:45:43 +0200

    Changed protected route authentication policy

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -2,7 +2,11 @@ import React from "react";
 import {Route, Redirect} from "react-router-dom";
 import Cookies from "js-cookie";
 
-const check_cookie = () => Cookies.get('sprinter-client');
+const check_cookie = () => {
+    //Cookies.get('sprinter-client'); // sometimes not working :C
+    const user = localStorage.getItem('user');
+    return user !== null;
+}
 
 export const ProtectedRoute = ({component: Component, ...rest}) => {
     return (
